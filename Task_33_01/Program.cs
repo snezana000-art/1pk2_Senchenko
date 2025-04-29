7936d4d3fa8afa64416c0a1704de19fd8f534a65
Снежана Сенченко <sencenkosnezana01@gmail.com>
2025-04-29 09:52:34 +0300

    Выпролнено задание 33 пары

---
+++ Task_33_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using static Task_33_01.Class1;
+
+namespace Task_33_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IDrawable triangle = new RightTriangle(5);
+                triangle.Draw();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+        
+    
+
+
---
+++ Task_33_01/RightTriangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_33_01
+{
+    internal class Class1
+    {
+        internal interface IDrawable
+        {
+            void Draw();
+        }
+
+        internal class RightTriangle : IDrawable
+        {
+            private int legsLength;
+            public int LegsLength
+            {
+                get => legsLength;
+                set
+                {
+                    if (value > 1)
+                        legsLength = value;
+                    else
+                        throw new ArgumentException("Длина катетов не должна быть меньше или равна 1");
+                }
+            }
+
+            public RightTriangle(int legsLength)
+            {
+                LegsLength = legsLength;
+            }
+
+            public void Draw()
+            {
+                Console.WriteLine();
+                for (int i = 0; i < LegsLength; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write('*');
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
